bb33f2e4968f9ad3bf377dc10bf6ce6250c390c2
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-18 15:21:26 -0300

    Definimos la estructura html del index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List</title>
+</head>
+<body>
+    <div class="to-do">
+        <form class="form" action="" method="POST">
+            <input class="form__input-text" type="text">
+            <input class="form__input-button" type="submit">
+        </form>
+        <div class="view-list">
+
+        </div>
+    </div>
+</body>
+</html>
