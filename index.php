84029ae0385a88f6f0fe000671782ee2d3ea6f40
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-18 16:12:19 -0300

    Terminamos de darle estilos al formulario

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
     <div class="to-do">
         <form class="form" action="" method="POST">
             <input class="form__input-text" type="text">
-            <input class="form__input-button" type="submit">
+            <input class="form__input-button" type="submit" value="Agregar">
         </form>
         <div class="view-list">
 
