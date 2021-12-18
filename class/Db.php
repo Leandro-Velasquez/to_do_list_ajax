ea9379d4cef4d72dd898312f799a3f719f7ccb6c
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-18 18:10:19 -0300

    Definimos el metodo __construct de la clase Db

--- class/Db.php
+++ class/Db.php
@@ -1,5 +1,20 @@
 <?php
     class Db {
-        
+
+        private $pdo;
+
+        public function __construct()
+        {
+            try{
+                $this->pdo = new PDO("mysql:host=localhost:3307;dbname=todolistdb;", "root", "");
+            }
+            catch(PDOException $e){
+                die("Error al conectar con la base de datos: " . $e->getMessage());
+            }catch(Exception $e){
+                die("Error generico: " . $e->getMessage());
+            }
+        }
+
+
     }
 ?>
