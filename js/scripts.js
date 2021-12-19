7c92dbf0c4f2600aa883b7853ecba95d2c680320
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-19 17:05:44 -0300

    Definimos la funcion mostrarTareas

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 const formulario = document.querySelector(".form");
+const viewList = document.querySelector(".view-list");
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -10,7 +11,22 @@ formulario.addEventListener("submit", (e) => {
     xml.addEventListener("load", (e) => {
         if(xml.status == 200){
             formulario.reset();
+            mostrarTareas();
         }
-    })
+    });
     xml.send(datosFormulario);
 });
+
+const mostrarTareas = () => {
+    const xml = new XMLHttpRequest();
+
+    xml.open("GET", "./class/procesar_tarea.php", true);
+    xml.addEventListener("load", () => {
+        if(xml.status == 200){
+            viewList.innerHTML = xml.responseText;
+        }
+    });
+    xml.send();
+}
+
+mostrarTareas();
