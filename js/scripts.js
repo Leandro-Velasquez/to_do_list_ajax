911dfd821423490106713257bec734d784f6bf39
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-20 19:58:03 -0300

    Se definio la funcion checkTarea

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,25 @@ const mostrarTareas = () => {
     xml.addEventListener("load", () => {
         if(xml.status == 200){
             viewList.innerHTML = xml.responseText;
+            checkTarea();
         }
     });
     xml.send();
 }
 
 mostrarTareas();
+
+const checkTarea = () => {
+    const lista = document.querySelector(".lista");
+    lista.addEventListener("click", (e) => {
+        if(e.target.className == "fas fa-check"){            
+            const xml = new XMLHttpRequest();
+            xml.open("POST", "./class/procesar_tarea.php", true);
+            xml.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+            xml.addEventListener("load", () => {
+                mostrarTareas();
+            })
+            xml.send("tareaRealizada=true&id=" + e.target.parentElement.children[0].id);
+        }
+    })
+}
