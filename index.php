730c6e26b0e9be87a8072db239f31e0f28b7fc84
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-20 18:09:08 -0300

    Le dimos estilos a la lista de tareas pendientes

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css?9">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
     <div class="to-do">
-        <form class="form" action="" method="POST">
-            <input class="form__input-text" type="text">
+        <form class="form" method="POST">
+            <input class="form__input-text" name="tarea" type="text" autocomplete="off">
             <input class="form__input-button" type="submit" value="Agregar">
         </form>
         <div class="view-list">
 
         </div>
     </div>
-    <script src="scripts.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 </html>
