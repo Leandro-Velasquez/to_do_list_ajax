e8fd4eec68bdb958a8464975553956556aea1db8
Leandro Velasquez <Leandro.velasquez032@gmail.com>
2021-12-21 23:55:04 -0300

    Definimos un else if para mostrar solo las tareas que ya fueron realizadas

--- class/procesar_tarea.php
+++ class/procesar_tarea.php
@@ -10,6 +10,9 @@
     else if(isset($_POST["tareaRealizada"]) && $_POST["tareaRealizada"] == true){
         $db->tareaRealizada($_POST["id"]);
     }
+    else if(isset($_POST["mostrarTareasRealizadas"])){
+        echo $db->mostrarTareasRealizadas();
+    }
     else{
         echo $db->mostrarTareasPendientes();
     }
